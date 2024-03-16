8c3708dfa6573778f0e8905961dd432b28e4f6f4
mahcodes <mhmdali102@bk.ru>
2024-03-16 02:04:41 +0200

    Create LICENSE

--- src/ui/prestyled.ts
+++ src/ui/prestyled.ts
@@ -1,3 +1,26 @@
+/**
+ * Sovereign Computing For a Brighter Future
+ * Copyright (C) 2024 Nodetec.co
+ *
+ * This file is part of Hackr News.
+ *
+ * Hackr News is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Hackr News is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hackr News. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Contact information:
+ * chris.machine@pm.me
+ */
+
 import { twJoin } from "tailwind-merge";
 
 // This applies shadow and animations to all small floats (menu, dropdown, tooltip, ...)
