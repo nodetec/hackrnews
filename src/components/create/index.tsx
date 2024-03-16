adadaea83f9f13647210d8bcc6acd9752681f7be
mahcodes <mhmdali102@bk.ru>
2024-03-16 02:40:32 +0200

    Ceate LICENSE

--- src/components/create/index.tsx
+++ src/components/create/index.tsx
@@ -1,3 +1,26 @@
+/**
+ * Sovereign Computing For a Brighter Future
+ * Copyright (C) 2024 Nodetec.co
+ *
+ * This file is part of HackrNews.
+ *
+ * HackrNews is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HackrNews is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HackrNews. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Contact information:
+ * luis..f.carvalho20+hackrnews@gmail.com
+ */
+
 import { CREATE_ROUTES } from "@/utils/routes/create";
 import Route from "./route";
 
